bf519d914b97088e15a0eb20b3e07c5194932870
Lee Macri <lem@rathkeale.school.nz>
2021-03-30 09:21:23 +1300

    Remove players

--- GroupBlackJack/Program.vb
+++ GroupBlackJack/Program.vb
@@ -9,26 +9,22 @@ Module Program
 
         ' Create Player And dealer
         Console.WriteLine("Please enter your name.")
-        Dim PlayerName As String = Console.ReadLine()
-        Dim Player1 As New Player(PlayerName)
-        Dim Dealer As New Player("Dealer")
 
         ' Create two BlackjackHands, PlayerHand And DealerHand
-        Dim PlayerHand As New Hand()
-        Dim DealerHand As New Hand()
+       
 
         ' Keep playing until the player quits Or a player reaches 20 points
         Deck.Shuffle()
         '   Deal two cards into each hand
-        PlayerHand.AddCard(Deck.DealCard())
-        DealerHand.AddCard(Deck.DealCard())
-        PlayerHand.AddCard(Deck.DealCard())
-        DealerHand.AddCard(Deck.DealCard())
-
+        
         '   Check If dealer has blackjack (if so, game ends)
+        
         '   Check If user has blackjack (if so, game ends)
+        
         '   User draws cards (If user goes over 21, game ends)
+        
         '   Dealer draws cards
+        
         '   Check for winner
         ' Loop
 
